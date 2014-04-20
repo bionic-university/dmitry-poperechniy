1f0f2ffce69170b23e44ff6775d92832532d8dbc
Dmytro Poperechnyy <adifucan@gmail.com>
2014-04-20 11:54:22 +0000

    New funtions in functions.php and updated index.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * 26. Найти пропущенный элемент в массиве, в котором есть все числа от 0 до 99 кроме одного (его и найти)
  *
  * @param array $arr
- * @return int
+ * @return float
  */
 function findMissingNumber($arr) {
     if (!$arr) {
@@ -29,4 +29,73 @@ function findMissingNumber($arr) {
     }
 
     return $total;
+}
+
+/**
+ * 18) Найти моду чисел в массиве
+ *
+ * @param array $arr
+ * @return mixed
+ */
+
+function findMode1(array $arr) {
+    $values = array_count_values($arr);
+    $mode = array_search(max($values), $values);
+
+    return $mode;
+}
+
+/**
+ * Returns the mode value(s) for an array.
+ *
+ * @param array $arr The array of values to determine the mode.
+ * @return array|bool Return mode value if there is only one, array of values if bimodel or FALSE if no mode.
+ */
+function findMode2(array $arr) {
+    if (!is_array($arr)) {
+        return false;
+    }
+
+    $values = array_count_values($arr);
+    //sort values in descending order
+    arsort($values);
+    $modes = array_keys($values, current($values), TRUE);
+
+    // If each value only occurs once, there is no mode
+    if (count($arr) === count($values))
+        return FALSE;
+
+    // Only one modal value
+    if (count($modes) === 1)
+        return $modes[0];
+
+    // Multiple modal values
+    return $modes;
+}
+
+/**
+ * 18) Найти медиану чисел в массиве
+ *
+ * @param $array
+ * @return bool|float|null
+ */
+function findMedian($array) {
+    if (!is_array($array)) {
+        return false;
+    }
+    $count = count($array);
+
+    if ($count == 0) {
+        return null;
+    }
+    sort($array, SORT_NUMERIC);
+
+    $middleIndex = floor($count / 2);
+    $median = $array[$middleIndex];
+    //if odd $count, middle is the median
+    if ($count % 2 != 0) {
+        return $median;
+    }
+    //if even $count, calculate avg of 2 medians
+    return ($median + $array[$middleIndex-1]) / 2;
 }
--- index.php
+++ index.php
@@ -7,6 +7,20 @@ $a = [1, 2, 3, 4, 5, 6, 7, 8, 10, 9, 12];
 
 echo "1. Missing number in array: ".findMissingNumber($a)."<br/>";
 
+$a2 = [1, 2, 3, 4, 3, 5, 6, 7, 8, 10, 9, 12, 4, 5, 6, 1, 5, 5, 78,6 ,8, 45, 5, 5];
+
+echo "2. Find mode in array: ".findMode1($a2)."<br/>";
+
+$single_mode = ['a', 'b', 'a', 'b', 'c', 'a'];
+$multi_mode  = [0, 0, 2, 2, 1, 1, 3];
+$no_mode     = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+echo "2. Find mode in array V2: ".findMode2($single_mode)."<br/>";
+
+$a3 = [5, 1, 3, 25, 19, 17, 21, 19];
+
+echo "3. Find median in array: ".findMedian($a3)."<br/>";
+
 
 
 
