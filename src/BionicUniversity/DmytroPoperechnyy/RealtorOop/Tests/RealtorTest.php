cf77af1f2ecd28f60cb230b1f2764299e6428f1d
Dmytro Poperechnyy <adifucan@gmail.com>
2014-06-01 14:49:50 +0300

    RealtorTest modified

--- src/BionicUniversity/DmytroPoperechnyy/RealtorOop/Tests/RealtorTest.php
+++ src/BionicUniversity/DmytroPoperechnyy/RealtorOop/Tests/RealtorTest.php
@@ -2,7 +2,7 @@
 
 namespace BionicUniversity\DmytroPoperechnyy\RealtorOop\Tests;
 
-use BionicUniversity\DmytroPoperechnyy\RealtorOop\Code;
+use BionicUniversity\DmytroPoperechnyy\RealtorOop\Code\Realtor;
 
 /**
  * Class RealtorTest
@@ -13,9 +13,42 @@ class RealtorTest extends \PHPUnit_Framework_TestCase
     /**
      * Test getting Realtor's name
      */
-    public function testSetName()
+    public function testGetName()
     {
-        $realtor = new Code\Realtor('John Doe', 'West str. 123, Los Angeles');
+        $realtor = new Realtor('John Doe', 'West str. 123, Los Angeles');
         $this->assertEquals('John Doe', $realtor->getName());
     }
+
+    /**
+     * Test getting Realtor's contact info
+     */
+    public function testGetContact()
+    {
+        $realtor = new Realtor('Jane Doe', 'Sunset blvr. 987, Miami, USA');
+        $this->assertEquals('Sunset blvr. 987, Miami, USA', $realtor->getContact());
+    }
+
+    /**
+     * Test setting name
+     */
+    public function testSetName()
+    {
+        $realtor = new Realtor('Johny Walker', 'East str. 234, New York, USA');
+        $name = "Homer Simpson";
+        $realtor->setName($name);
+        $this->assertEquals($name, $realtor->getName());
+    }
+
+    /**
+     * Test setting contact
+     */
+    public function testSetContact()
+    {
+        $realtor = new Realtor('Crusty', 'South str. 567, Boston, USA');
+        $contact = 'North str. 876, Springfield, USA';
+        $realtor->setContact($contact);
+        $this->assertEquals($contact, $realtor->getContact());
+    }
+
+
 }
